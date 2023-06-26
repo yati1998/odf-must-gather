518ec9fa1f74faf6c27ce71c36f2472569cdbadf
black-dragon74 <niryadav@redhat.com>
2023-06-26 06:55:05 +0530

    utils: allow piped input to debug functions
    Signed-off-by: black-dragon74 <niryadav@redhat.com>

--- must-gather/collection-scripts/utils.sh
+++ must-gather/collection-scripts/utils.sh
@@ -3,10 +3,27 @@
 #
 # Please Note:
 # While using bash variables inside the functions defined here,
-# ensure that the variable has been defined before you,
-# call the realted helper function.
+# ensure that the variable has been defined before,
+# you call the realted helper function.
 #
 
 dbglog() {
-    echo -e "${1}" | tee -a "${BASE_COLLECTION_PATH}"/gather-debug.log
+    # Allow the input to be piped
+    declare msg=${1:-$(</dev/stdin)}
+
+    echo -e "${msg}" | tee -a "${BASE_COLLECTION_PATH}"/gather-debug.log
+}
+
+# Works similar to dbglog but accepts a custom file name as an arg
+# 1st arg is the file name to write to
+# 2nd arg is the message to append
+dbglogf() {
+    # Allow the input to be piped
+    declare msg=${2:-$(</dev/stdin)}
+
+    if [[ -z $1 || -z $msg ]]; then
+        echo "dbglogf: Not enough args to call the function."
+    fi
+
+    echo -e "${msg}" | tee -a "${1}"
 }
