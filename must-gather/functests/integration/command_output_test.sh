8d16d47eb424d31017596469f2456fa5ea52f790
black-dragon74 <niryadav@redhat.com>
2023-07-12 14:49:36 +0530

    Use bash expressions to check for null/non-null
    Signed-off-by: black-dragon74 <niryadav@redhat.com>

--- must-gather/functests/integration/command_output_test.sh
+++ must-gather/functests/integration/command_output_test.sh
@@ -6,7 +6,7 @@
 BASE_PATH=$1
 
 # Use PWD as base path if no argument is passed
-if [ "${BASE_PATH}" = "" ]; then
+if [ -z "${BASE_PATH}" ]; then
     BASE_PATH=$(pwd)
 fi
 
@@ -15,17 +15,16 @@ fi
 for path in $(find "${BASE_PATH}" -type d -name must_gather_commands); do
     numberOfEmptyOutputFiles=$(find "${path}" -empty -type f | wc -l)
     if [ "${numberOfEmptyOutputFiles}" -ne "0" ]; then
-        printf "The following files must not be empty : \n\n";
-        find "${path}" -empty -type f 
-        exit 1;
+        printf "The following files must not be empty : \n\n"
+        find "${path}" -empty -type f
+        exit 1
     fi
 
     jsonOutputPath=${path}/json_output
     numberOfEmptyOutputFiles=$(find "${jsonOutputPath}" -empty -type f | wc -l)
     if [ "${numberOfEmptyOutputFiles}" -ne "0" ]; then
-        printf "The following files must not be empty : \n\n";
-        find "${jsonOutputPath}" -empty -type f 
-        exit 1;
+        printf "The following files must not be empty : \n\n"
+        find "${jsonOutputPath}" -empty -type f
+        exit 1
     fi
 done
-
