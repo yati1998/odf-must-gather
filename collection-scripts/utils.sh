a35de6820758c4bf2eb6d793d0509c7aa1152c7d
black-dragon74 <niryadav@redhat.com>
2024-03-29 15:24:04 +0530

    Add support for --since and --since-time flags
    The must-gather now filters its logs based on the above
    parameters. The said parameters are provided by the OC cli
    as ENV values.
    
    Signed-off-by: black-dragon74 <niryadav@redhat.com>

--- collection-scripts/utils.sh
+++ collection-scripts/utils.sh
@@ -32,6 +32,33 @@ dbglogf() {
     echo -e "${msg}" | tee -a "${1}"
 }
 
+# Read the `MUST_GATHER_SINCE` and `MUST_GATHER_SINCE_TIME`
+# from the ENV and export it for use in the gather scripts
+#
+# Acc to spec, only one of `MUST_GATHER_SINCE`
+# or `MUST_GATHER_SINCE_TIME` can be used.
+parse_since_time() {
+    local FILTER_ARGS=""
+
+    # We do not need to validate the values here, they are validated
+    # by the oc cli.
+    # Ref: https://github.com/openshift/oc/commit/1fc5c7d79ff5c2fa19c35b7a004dc99eb3330806
+    if [ -n "${MUST_GATHER_SINCE:-}" ]; then
+        FILTER_ARGS="--since=${MUST_GATHER_SINCE}"
+    fi
+
+    if [ -n "${MUST_GATHER_SINCE_TIME:-}" ]; then
+        FILTER_ARGS="--since-time=${MUST_GATHER_SINCE_TIME}"
+    fi
+
+    # It is possible that user is not providing any filters
+    # We do not export the variable in that case
+    if [ -n "${FILTER_ARGS}" ]; then
+        export LOG_FILTER_ARGS="${FILTER_ARGS}"
+    fi
+}
+
 # Export the functions so that the file needs to be sourced only once
 export -f dbglog
 export -f dbglogf
+export -f parse_since_time
